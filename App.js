5926883558114eb1cadfcad3e277eaae61646ac0
bimaagung <bimaagung.labs@gmail.com>
2018-10-04 21:04:26 +0700

    textInput Fix

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { 
+          AppRegistry, 
+          StyleSheet, 
+          Text, 
+          View, 
+          TextInput,
+          TouchableOpacity 
+} from 'react-native';
 import { createStackNavigator } from 'react-navigation'
 
 class InputUsers extends Component{
@@ -17,10 +24,36 @@ class InputUsers extends Component{
   //   }
   // }
 
+  // InsertUsers = () => {
+  //   const {TextInputName} = this.state;
+  //   const {TextInputEmail} = this.state;
+  //   const {TextInputPhoneNumber} = this.state;
+  // }
+
   render() {
     return (
       <View style={styles.container}>
-        
+        <TextInput
+          placeholder = "Enter Name"
+          onChangeText = {TextInputValue => this.setState({TextInputName: TextInputValue})}
+          underlineColorAndroid = 'transparent'
+          style = {styles.TextInputStyle2}
+        />
+        <TextInput
+          placeholder = "Enter Email"
+          onChangeText = {TextInputValue => this.setState({TextInputEmail: TextInputValue})}
+          underlineColorAndroid = 'transparent'
+          style = {styles.TextInputStyle}
+        />
+        <TextInput
+          placeholder = "Enter PhoneNumber"
+          onChangeText = {TextInputValue => this.setState({TextInputPhoneNumber: TextInputValue})}
+          underlineColorAndroid = 'transparent'
+          style = {styles.TextInputStyle}
+        />
+        <TouchableOpacity activeOpacity = {.4} style = {styles.TouchableOpacityStyle} onPress={this.InputUsers}>
+            <Text>Save</Text>
+        </TouchableOpacity>
       </View>
     );
   }  
@@ -39,6 +72,27 @@ const styles = StyleSheet.create({
     marginTop: 5,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  TextInputStyle: {
+    textAlign:'center',
+    marginBottom: 7,
+    width: '90%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#FF5722',
+  },
+  TextInputStyle2: {
+    textAlign:'center',
+    marginBottom: 7,
+    width: '90%',
+    marginTop: 20,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#FF5722',
+  },
+  TouchableOpacityStyle: {
+
+  }
 });
